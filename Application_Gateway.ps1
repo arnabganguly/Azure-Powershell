6b283060db9a4c5d38202cd82cc10a320d4444f1
arnabganguly <arnab.ganguly@yahoo.com>
2017-06-03 12:48:22 -0700

    Initial Commit

---
+++ Application_Gateway.ps1
@@ -0,0 +1,16 @@
+﻿Login-AzureRmAccount
+
+* Create a new subnet
+$subnet = New-AzureRmVirtualNetworkSubnetConfig -Name agsubnetps -AddressPrefix 10.0.4.0/24 
+$vnet = New-AzureRmVirtualNetwork -Name agvmps -ResourceGroupName agrg -Location "West Central US" -AddressPrefix 10.0.0.0/16 -Subnet $subnet
+$subnet = $vnet.Subnets[0]
+
+$publicip = New-AzureRmPublicIpAddress -Name appipps -ResourceGroupName agrg -Location "West Central US" -AllocationMethod Dynamic
+
+# Create the application gateway IP Configuration 
+$gipconfig = New-AzureRmApplicationGatewayIPConfiguration -Name aggateway101 -subnet $subnet 
+
+# Create backend IP address pool 
+$pool = New-AzureRmApplicationGatewayBackendAddressPool -Name agapplicationpool -BackendIPAddresses 10.0.1.4,10.0.2.4
+
+# Create the front end 
---
+++ Metric_Reporting.ps1
@@ -0,0 +1,7 @@
+﻿Login-AzureRmAccount
+Get-AzureRmAlertRule -ResourceGroup agrg -DetailedOutput
+$actionemail = New-AzureRmAlertRuleEmail -CustomEmails arnab.ganguly@hcl.com
+$actionwebhook = New-AzureRmAlertRuleWebhook -ServiceUri https://www.contoso.com?token=mytoken
+
+Add-AzureRmMetricAlertRule -Name PSmonitoringrule -Location "West US" -ResourceGroup agrg -TargetResourceId /subscriptions/8da2f62c-3648-4ad5-9537-b7d8a1f166e5/resourceGroups/agrg/providers/Microsoft.Compute/virtualMachines/agwebvm -MetricName "Percentage CPU" -Operator GreaterThan -Threshold 60 -WindowSize 00:05:00 -TimeAggregationOperator Total -Actions $actionemail,$actionwebhook -Description "alert on website activity" 
+Get-AzureRmAlertRule -ResourceGroup agrg -DetailedOutput
